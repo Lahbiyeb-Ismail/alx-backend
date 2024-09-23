b549ce0a60f38737ab1724b8b7ff72d36f679327
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-23 14:10:44 +0100

    11. Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -8,18 +8,18 @@ function createPushNotificationsJobs(jobs, queue) {
     const job = queue.create('push_notification_code_3', jobData)
       .save((err) => {
         if (!err) {
-          console.log(`Notification job created: ${job.id}`);
+          console.log(`Notification job created: ${job.id || 'test mode'}`);
         }
       });
 
     job.on('complete', () => {
-      console.log(`Notification job ${job.id} completed`);
+      console.log(`Notification job ${job.id || 'test mode'} completed`);
     }).on('failed', (err) => {
-      console.log(`Notification job ${job.id} failed: ${err}`);
+      console.log(`Notification job ${job.id || 'test mode'} failed: ${err}`);
     }).on('progress', (progress) => {
-      console.log(`Notification job ${job.id} ${progress}% complete`);
+      console.log(`Notification job ${job.id || 'test mode'} ${progress}% complete`);
     });
   });
 }
 
-export default createPushNotificationsJobs;
+module.exports = createPushNotificationsJobs;
