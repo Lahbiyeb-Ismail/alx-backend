21d6c50c873236af99e86001403d3997332d4869
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-23 13:44:59 +0100

    4. Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.hset('HolbertonSchools', 'Portland', 50, print);
+client.hset('HolbertonSchools', 'Seattle', 80, print);
+client.hset('HolbertonSchools', 'New York', 20, print);
+client.hset('HolbertonSchools', 'Bogota', 20, print);
+client.hset('HolbertonSchools', 'Cali', 40, print);
+client.hset('HolbertonSchools', 'Paris', 2, print);
+
+// Display Hash
+client.hgetall('HolbertonSchools', (err, obj) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('HolbertonSchools:', obj);
+  }
+  client.quit();
+});
